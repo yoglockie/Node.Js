18a33c08488e1e3046276658e62f36928ca87832
yoglockie <yogilokesh916@gmail.com>
2023-08-25 15:14:19 +0530

    data file re add

---
+++ data.js
@@ -0,0 +1,34 @@
+const data = [
+    {   id:1,
+        title:'Steve McCurry',
+        imgUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Steve_McCurry_%285824371040%29.jpg/1200px-Steve_McCurry_%285824371040%29.jpg',
+        desc:'Nature Images & Pictures. We handpicked more than 1,200,000 nature pictures for your choosing. HD to 4K quality images, all for free! Royalty-free images.'
+        
+    },
+    {
+        id:2,
+        title:'Robert Capa',
+        imgUrl:'https://ychef.files.bbci.co.uk/976x549/p01k3q5h.jpg',
+        desc:'Nature Images & Pictures. We handpicked more than 1,200,000 nature pictures for your choosing. HD to 4K quality images, all for free! Royalty-free images.'
+        
+    },
+    {   id:3,
+        title:'Annie Leibovitz',
+        imgUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFDalxDqggDga6hLrQYrIq_9_zAjith1ke4Q&usqp=CAU',
+        desc:'Nature Images & Pictures. We handpicked more than 1,200,000 nature pictures for your choosing. HD to 4K quality images, all for free! Royalty-free images.'
+        
+    },
+    {
+        id:4,
+        title:'Elliott Erwitt',
+        imgUrl:'https://cdn.britannica.com/20/183320-050-F81B2153/Elliott-Erwitt-2014.jpg',
+        desc:'Nature Images & Pictures. We handpicked more than 1,200,000 nature pictures for your choosing. HD to 4K quality images, all for free! Royalty-free images.'
+        
+    },
+    
+    
+   
+    
+]
+
+export default data;
